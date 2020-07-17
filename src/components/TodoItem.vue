6067ea168c7233a38e41c66ed91fa0b29a2d3926
“Abraham <mrabblessedslyst4christ099@gmail.com>
2020-07-17 17:43:30 +0300

    Add item feature

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        {{todo.title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 0;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        padding: 5px;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
---
+++ vue.js/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data(){
+    
+  }
+}
+</script>
+
+<style>
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+    body{
+      font-family: Arial, Helvetica, sans-serif;
+      line-height: 1.4;
+    }
+</style>
